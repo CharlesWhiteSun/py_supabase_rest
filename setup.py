e3bf9da834079d5d6ff56febf2aedf103fb305ba
charleswhitesun <charleswhitesun@gmail.com>
2025-09-29 10:15:00 +0800

    Added setup version number control

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="py_supabase_rest",
+    version="1.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "requests",
+        # 其他依賴套件
+    ],
+    description="A Python client for Supabase REST API",
+    url="https://github.com/CharlesWhiteSun/py_supabase_rest",
+    author="Charles",
+    author_email="charleswhitesun@gmail.com",
+    license="Apache License 2.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
